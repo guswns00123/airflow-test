0fe2f7e38fbddce6ac3d0cf78ccf58c5bae3845d
Yoo <fresh0911@naver.com>
2023-08-11 19:36:14 +0900

    python args op 추가

---
+++ dags/dags_python_with_op_args.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.python import PythonOperator
+from common.common_fuc import regist
+
+with DAG(
+    dag_id="dags_python_import_func",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag: 
+    task_regist = PythonOperator(
+        task_id = 'task_regist',
+        python_callable=regist,
+        op_args = ['hjyoo', 'man', 'kr', 'seoul']
+    )
+
+    task_regist
