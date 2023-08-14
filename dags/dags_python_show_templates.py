95f26ab2287f2a504e89502e30d245f3bb9c9c6b
Yoo <fresh0911@naver.com>
2023-08-14 19:01:07 +0900

    추가

--- dags/dags_python_show_templates.py
+++ dags/dags_python_show_templates.py
@@ -7,7 +7,7 @@ from airflow.decorators import task
 with DAG(
     dag_id="dags_python_show_templates",
     schedule="10 0 * * *",
-    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2023, 8, 10, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     @task(task_id='python_task')
