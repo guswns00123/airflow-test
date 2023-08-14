0dc8a3313083b090c920b9254e245463c6c0a0d5
Yoo <fresh0911@naver.com>
2023-08-14 18:58:06 +0900

    날짜 관련 airflow 출력 업로드

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_python_show_templates",
+    schedule="10 0 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    @task(task_id='python_task')
+    def show_templates(**kwargs):
+        from pprint import pprint
+        pprint(kwargs)
+
+    show_templates()
