081a9b42b8949afbc64532154e02e02ab068cdd1
Yoo <fresh0911@naver.com>
2023-08-16 18:22:45 +0900

    branch upload

---
+++ dags/dags_bash_with_variable.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.bash import BashOperator
+from airflow.models import Variable
+
+with DAG(
+    dag_id="dags_bash_with_variable",
+    schedule="10 9 * * *",
+    start_date=pendulum.datetime(2023, 4, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    var_value = Variable.get("sample_key")
+
+    bash_var_1 = BashOperator(
+    task_id="bash_var_1",
+    bash_command=f"echo variable:{var_value}"
+    )
+
+
+    ##이 방법을 더 권고
+    ## 스케줄러 부하를 더 경감시켜줌
+    bash_var_2 = BashOperator(
+    task_id="bash_var_2",
+    bash_command="echo variable:{{var.value.sample_key}}"
+    )
