4fa8bc30da5d3193395446aff811dc0f563b2859
Yoo <fresh0911@naver.com>
2023-08-17 20:06:42 +0900

    추가

--- dags/dags_seoul_api_corona.py
+++ dags/dags_seoul_api_corona.py
@@ -11,14 +11,14 @@ with DAG(
     tb_corona19_count_status = SeoulApiTocsvOperator(
         task_id = 'tb_corona19_count_status',
         dataset_nm='TbCorona19CountStatus',
-        path = '/opt/airflow-test/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul")  | ds_nodash}}',
+        path = '/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul")  | ds_nodash}}',
         filename='TbCorona19CountStatus.csv'
     )
 
     tv_corona19_vaccine_stat_new = SeoulApiTocsvOperator(
         task_id = 'tv_corona19_vaccine_stat_new',
         dataset_nm= 'tvCorona19VaccinestatNew',
-        path='/opt/airflow-test/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul) | ds_nodash}}',
+        path='/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul) | ds_nodash}}',
         filename='tvCorona19VaccinestatNew.csv'
     )
 
