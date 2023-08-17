42765111780306e41e9e9a2295c8291b43191364
Yoo <fresh0911@naver.com>
2023-08-17 19:20:39 +0900

    수정

--- dags/dags_simple_http_operator.py
+++ dags/dags_simple_http_operator.py
@@ -16,7 +16,7 @@ with DAG(
         http_conn_id='openapi.seoul.go.kr',
         endpoint='{{var.value.apikey_openapi_seoul_go_kr}}/json/SeoulLibraryLectureInfo/1/5/',
         mehtod='GET',
-        header={'Content-Type': 'application/json',
+        headers={'Content-Type': 'application/json',
                 'charset': 'utf-8',
                 'Accept': '*/*'}
 
