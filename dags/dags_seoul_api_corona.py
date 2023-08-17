e994c98e60f6e793dbd0bf84b12c96a0a1686f7c
Yoo <fresh0911@naver.com>
2023-08-17 20:28:56 +0900

    6

--- dags/dags_seoul_api_corona.py
+++ dags/dags_seoul_api_corona.py
@@ -5,14 +5,14 @@ import pendulum
 with DAG(
     dag_id='dags_seoul_api_corona',
     schedule='0 7 * * *',
-    start_date=pendulum.datetime(2023,8,1, tz='Asia/Seoul'),
+    start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
     catchup=False
 ) as dag:
     '''서울시 코로나19 확진자 발생동향'''
     tb_corona19_count_status = SeoulApiToCsvOperator(
         task_id='tb_corona19_count_status',
         dataset_nm='TbCorona19CountStatus',
-        path='/opt/airflow-test/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
+        path='/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
         file_name='TbCorona19CountStatus.csv'
     )
     
@@ -20,7 +20,7 @@ with DAG(
     tv_corona19_vaccine_stat_new = SeoulApiToCsvOperator(
         task_id='tv_corona19_vaccine_stat_new',
         dataset_nm='tvCorona19VaccinestatNew',
-        path='/opt/airflow-test/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
+        path='/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
         file_name='tvCorona19VaccinestatNew.csv'
     )
 
