730b4b0563dd2b8f8922d3e5bb469d7b4151d678
Yoo <fresh0911@naver.com>
2023-08-21 19:32:59 +0900

    111

--- dags/dags_seoul_api_corona.py
+++ dags/dags_seoul_api_corona.py
@@ -8,7 +8,7 @@ with DAG(
     start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
     catchup=False
 ) as dag:
-    '''서울시 코로나19 확진자 발생동향'''
+    '''서울시 코로나19 확진자 발생'''
     tb_corona19_count_status = SeoulApiToCsvOperator(
         task_id='tb_corona19_count_status',
         dataset_nm='TbCorona19CountStatus',
